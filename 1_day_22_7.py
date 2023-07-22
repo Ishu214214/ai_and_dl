8faf79de984dec0c4031eef920bdc9286d1c540b
ishu <ishukr150@gmail.com>
2023-07-22 10:11:16 +0530

    first commite

--- 1_day_22_7.py
+++ 1_day_22_7.py
@@ -1,7 +1,10 @@
-git init
+# git init
 
 
-git commit --global user.name "ishu kumar"
-git commit --global user.email "2020pietcsishu80@poornima.org"
-git add .
-#iunbjnj
+# git commit --global user.name "ishu kumar"
+# git commit --global user.email "2020pietcsishu80@poornima.org"
+# git add .
+
+# git commit -m "first commit"
+
+# git remote add origin https://github.com/Ishu214214/ai_and_dl.git
